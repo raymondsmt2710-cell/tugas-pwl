3f838147a20151f4f2f52205ebc1d80c40d0197a
yazri211 <khoiriyazri@gmail.com>
2026-06-07 16:27:32 +0700

    add delete comments

--- app/Models/CampaignComment.php
+++ app/Models/CampaignComment.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CampaignComment extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'id_campaign',
         'id_user',
