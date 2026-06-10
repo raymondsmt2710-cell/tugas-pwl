c82f7ef14ff468e22f2cbeb52f0c254217aa809b
yazri211 <khoiriyazri@gmail.com>
2026-06-10 10:31:27 +0700

    fix profile picture bug

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="referrer" content="no-referrer">
     <title>{{ $title ?? 'Autopahala' }}</title>
     <link rel="icon" type="image/jpeg" href="{{ asset('images/favicon.jpeg') }}">
     <link rel="shortcut icon" type="image/jpeg" href="{{ asset('images/favicon.jpeg') }}">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="referrer" content="no-referrer">
 
         <title>{{ $title ?? config('app.name', 'AutoPahala') }}</title>
 
